0cec0d709f9cbdb16a10dccce2b0fcbfbefc873a
NOS-Neuro <jer.west88@gmail.com>
2026-03-25 00:50:41 -0400

    Remove @astrojs/vercel dependency from package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
   integrations: [
     react(),
     tailwind({
